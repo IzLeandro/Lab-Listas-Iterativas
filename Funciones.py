5b225a81ea29c6b10b13b155b04ecbfeab5535bd
IzLeandro <34462612+IzLeandro@users.noreply.github.com>
2020-10-20 10:24:34 -0600

    buscarElemento added
    comentarios faltan**

--- Funciones.py
+++ Funciones.py
@@ -2,16 +2,27 @@ def separarElementos(list):
     #variables
     impar=[]
     par=[]
-    #comprobacion
+    #comprobacion n1
     if list==[]:
         return ("La lista de números no debe ser vacía.")
     for  elemento in list:
+        #comprobacion n2
         if type(elemento) == str:
-            return ("Elementos no numericos en lista")
+            return ("Elementos no numericos en lista.")
         if elemento%2==0:
             par=par+[elemento]
         else:
             impar= impar + [elemento]
     return [par,impar]
 
-print(separarElementos([2,4,6,8]))
+def buscarElemento(list,find=None):
+    #variables
+    res=0
+    #validaciones
+    if find==None:
+        return ('Debe indicar el numero que desea buscar.')
+    for elemento in list:
+        if elemento == find:
+            res+=1
+    return res
+
