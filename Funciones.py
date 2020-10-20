760681a7fd766488f7c046a868cbe55ea7fa794c
IzLeandro <34462612+IzLeandro@users.noreply.github.com>
2020-10-20 11:13:15 -0600

    CrearlistaVocales, crearlista, insertarElemento and eliminarRepetidos added

--- Funciones.py
+++ Funciones.py
@@ -1,13 +1,13 @@
+#importaciones
+import random
 def separarElementos(list):
     #variables
     impar=[]
     par=[]
-    #comprobacion n1
-    if list==[]:
+    if list==[]:    #comprobacion n1
         return ("La lista de números no debe ser vacía.")
     for  elemento in list:
-        #comprobacion n2
-        if type(elemento) == str:
+        if type(elemento) == str:        #comprobacion n2
             return ("Elementos no numericos en lista.")
         if elemento%2==0:
             par=par+[elemento]
@@ -18,11 +18,47 @@ def separarElementos(list):
 def buscarElemento(list,find=None):
     #variables
     res=0
-    #validaciones
-    if find==None:
+    if find==None:    #validaciones
         return ('Debe indicar el numero que desea buscar.')
     for elemento in list:
         if elemento == find:
             res+=1
     return res
+
+def crearListaVocales(list):
+    #variables
+    consVocales=['a','e','i','o','u']
+    resultVocales=[]
+    for elemento in list:
+        if type(elemento) != str: #comprobacion
+            continue
+        if elemento in consVocales:
+            resultVocales=resultVocales+[elemento]
+    return resultVocales
+
+def crearLista(num):
+    #variables
+    lista=[]
+    while num!=0:
+        lista=lista + [random.randint(1,99)]
+        num-=1
+    print("Se generó la lista",lista,", El mayor es:", max(lista) ,"El menor es:", min(lista) ,"y hay una diferencia de:",(max(lista)-min(lista)))
+    return ""
+
+def insertarElemento(elem1,elem2,lista):
+    #variables
+    nuevaLista=[]
+    for i in lista:
+        nuevaLista= nuevaLista + [i]
+        if i == elem1:
+            nuevaLista = nuevaLista + [elem2]
+    return nuevaLista
+
+def eliminarRepetidos(lista):
+    #variables
+    nuevaLista=[]
+    for i in lista:
+        if i not in nuevaLista:
+            nuevaLista= nuevaLista + [i]
+    return nuevaLista
 
